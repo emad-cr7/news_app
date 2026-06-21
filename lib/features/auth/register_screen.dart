076d094d753b6a37d278daf628553dfb3dd76886
Emad <emadmarey29@gmail.com>
2026-06-21 14:58:40 +0300

    init project

--- lib/features/auth/register_screen.dart
+++ lib/features/auth/register_screen.dart
@@ -5,14 +5,9 @@ import '../../core/widget/Custom_elevated_button.dart';
 import '../../core/widget/Custom_text_from_field.dart';
 import '../home/home_screen.dart';
 
-class RegisterScreen extends StatefulWidget {
-  const RegisterScreen({super.key});
+class RegisterScreen extends StatelessWidget {
+   RegisterScreen({super.key});
 
-  @override
-  State<RegisterScreen> createState() => _RegisterScreenState();
-}
-
-class _RegisterScreenState extends State<RegisterScreen> {
   TextEditingController emailController = TextEditingController();
 
   TextEditingController passwordController = TextEditingController();
@@ -30,94 +25,99 @@ class _RegisterScreenState extends State<RegisterScreen> {
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage("assets/images/background.png"),
+            fit: BoxFit.cover,
           ),
         ),
         child: Padding(
           padding: const EdgeInsets.all(16),
           child: Form(
             key: _key,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Center(
-                  child: Image.asset("assets/images/logo.png", height: 45),
-                ),
-                SizedBox(height: 24),
-                Text(
-                  "Welcome to Newts",
-                  style: Theme.of(context).textTheme.displaySmall,
-                ),
-                SizedBox(height: 16),
-                CustomTextFromField(
-                  obscureText: false,
-                  controller: emailController,
-                  title: 'Email',
-                  hint: 'Enter Email',
-                  errorMessage: 'Please Enter Email',
-                ),
-                SizedBox(height: 12),
-                CustomTextFromField(
-                  obscureText: true,
-                  controller: passwordController,
-                  title: 'Password',
-                  hint: 'Enter Password',
-                  errorMessage: 'Please Enter Password',
-                ),
-                SizedBox(height: 12),
-                CustomTextFromField(
-                  obscureText:true,
-                  controller: confirmPasswordController,
-                  extraValidator: (value) {
-                    if (value != passwordController.text) {
-                      return "Passwords do not match";
-                    }
-                    return null;
-                  },
-                  title: 'Confirm Password',
-                  hint: 'Enter Confirm Password',
-                  errorMessage: 'Please Enter Confirm Password',
-
-                ),
-                SizedBox(height: 20),
-                Center(
-                  child: CustomElevatedButton(
-                    title: 'Sign Up',
-                    onPressed: () {
-                      if (_key.currentState!.validate()) {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (BuildContext context) {
-                              return LoginScreen();
-                            },
-                          ),
-                        );
-                      }
-                    },
-                  ),
-                ),
-                SizedBox(height: 20),
-                Row(
+            child: Center(
+              child: SingleChildScrollView(
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("Have an account ?"),
-                    TextButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (BuildContext context) {
-                              return LoginScreen();
-                            },
-                          ),
-                        );
+                    Center(
+                      child: Image.asset("assets/images/logo.png", height: 45),
+                    ),
+                    SizedBox(height: 24),
+                    Text(
+                      "Welcome to Newts",
+                      style: Theme.of(context).textTheme.displaySmall,
+                    ),
+                    SizedBox(height: 16),
+                    CustomTextFromField(
+                      obscureText: false,
+                      controller: emailController,
+                      title: 'Email',
+                      hint: 'Enter Email',
+                      errorMessage: 'Please Enter Email',
+                    ),
+                    SizedBox(height: 12),
+                    CustomTextFromField(
+                      obscureText: true,
+                      controller: passwordController,
+                      title: 'Password',
+                      hint: 'Enter Password',
+                      errorMessage: 'Please Enter Password',
+                    ),
+                    SizedBox(height: 12),
+                    CustomTextFromField(
+                      obscureText:true,
+                      controller: confirmPasswordController,
+                      extraValidator: (value) {
+                        if (value != passwordController.text) {
+                          return "Passwords do not match";
+                        }
+                        return null;
                       },
-                      child: Text("Sign Up"),
+                      title: 'Confirm Password',
+                      hint: 'Enter Confirm Password',
+                      errorMessage: 'Please Enter Confirm Password',
+
+                    ),
+                    SizedBox(height: 20),
+                    Center(
+                      child: CustomElevatedButton(
+                        title: 'Sign Up',
+                        onPressed: () {
+                          if (_key.currentState!.validate()) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (BuildContext context) {
+                                  return LoginScreen();
+                                },
+                              ),
+                            );
+                          }
+                        },
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Have an account ?"),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (BuildContext context) {
+                                  return LoginScreen();
+                                },
+                              ),
+                            );
+                          },
+                          child: Text("Sign Up"),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ],
+              ),
             ),
           ),
         ),
--- lib/features/auth/sign_in_screen.dart
+++ lib/features/auth/sign_in_screen.dart
@@ -7,16 +7,13 @@ import '../../core/widget/Custom_text_from_field.dart';
 import '../home/home_screen.dart';
 import '../main/main_screen.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+class LoginScreen extends StatelessWidget {
+   LoginScreen({super.key});
 
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
   TextEditingController emailController = TextEditingController();
+
   TextEditingController passwordController = TextEditingController();
+
   final GlobalKey<FormState> _key = GlobalKey();
 
   bool isPassword = false;
@@ -29,83 +26,88 @@ class _LoginScreenState extends State<LoginScreen> {
         height: double.infinity,
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage("assets/images/background.png"),
+            image: AssetImage("assets/images/background.png" , ),
+            fit: BoxFit.cover,
           ),
         ),
         child: Padding(
           padding: const EdgeInsets.all(16),
           child: Form(
             key: _key,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Center(
-                  child: Image.asset("assets/images/logo.png", height: 45),
-                ),
-                SizedBox(height: 24),
-                Text(
-                  "Welcome to Newts",
-                  style: Theme.of(context).textTheme.displaySmall,
-                ),
-                SizedBox(height: 16),
-                CustomTextFromField(
-                  controller: emailController,
-                  title: 'Email',
-                  hint: 'Enter Email',
-                  errorMessage: 'Please Enter Email',
-                ),
-                SizedBox(height: 12),
-                CustomTextFromField(
-                  obscureText: true,
-                  controller: passwordController,
-                  title: 'Password',
-                  hint: 'Enter Password',
-                  errorMessage: 'Please Enter Password',
-                ),
-                SizedBox(height: 20),
-                Center(
-                  child: CustomElevatedButton(
-                    title: 'Sign In',
-                    onPressed: () {
-                      if (_key.currentState!.validate()) {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (BuildContext context) {
-                              return MainScreen();
-                            },
-                          ),
-                        );
-                      }
-                    },
-                  ),
-                ),
-                SizedBox(height: 20),
-                Row(
+            child: Center(
+              child: SingleChildScrollView(
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("Don’t have an account ?"),
-                    SizedBox(width: 8),
-                    InkWell(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (BuildContext context) {
-                              return RegisterScreen();
-                            },
-                          ),
-                        );
-                      },
-                      child: Text(
-                        "Sign Up ",
-                        style: Theme.of(context).textTheme.titleSmall,
+                    Center(
+                      child: Image.asset("assets/images/logo.png", height: 45),
+                    ),
+                    SizedBox(height: 24),
+                    Text(
+                      "Welcome to Newts",
+                      style: Theme.of(context).textTheme.displaySmall,
+                    ),
+                    SizedBox(height: 16),
+                    CustomTextFromField(
+                      controller: emailController,
+                      title: 'Email',
+                      hint: 'Enter Email',
+                      errorMessage: 'Please Enter Email',
+                    ),
+                    SizedBox(height: 12),
+                    CustomTextFromField(
+                      obscureText: true,
+                      controller: passwordController,
+                      title: 'Password',
+                      hint: 'Enter Password',
+                      errorMessage: 'Please Enter Password',
+                    ),
+                    SizedBox(height: 20),
+                    Center(
+                      child: CustomElevatedButton(
+                        title: 'Sign In',
+                        onPressed: () {
+                          if (_key.currentState!.validate()) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (BuildContext context) {
+                                  return MainScreen();
+                                },
+                              ),
+                            );
+                          }
+                        },
                       ),
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Don’t have an account ?"),
+                        SizedBox(width: 8),
+                        InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (BuildContext context) {
+                                  return RegisterScreen();
+                                },
+                              ),
+                            );
+                          },
+                          child: Text(
+                            "Sign Up ",
+                            style: Theme.of(context).textTheme.titleSmall,
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ],
+              ),
             ),
           ),
         ),
